d73094b92ed8daeee364122e63597d6d7ae17f01
Daniel Tinivella <dtinivella@gmail.com>
2023-12-10 23:42:04 -0300

    add functions new_token and next_token -> Token to application

--- src/lexer.rs
+++ src/lexer.rs
@@ -7,16 +7,62 @@
 use crate::token::Token;
 
 struct Lexer {
-   input: Vec<char>,
+    input: Vec<char>,
+    position: usize,
+    read_position: usize,
+    ch: char,
 }
 
 impl Lexer {
    pub fn new(input: &str) -> Lexer {
-      todo!();
+      let mut lexer: Lexer = Lexer {
+         input: input.chars().collect(),
+         position: 0,
+         read_position: 0,
+         ch: Default::default(),
+        };
+
+        lexer.read_char();
+        lexer
    }
+
+   fn read_char(&mut self) {
+      if self.read_position >= self.input.len() {
+         self.ch = '\0';
+      } else {
+         self.ch = self.input[self.read_position];
+      }
+      self.position = self.read_position;
+      self.read_position += 1;
+   }
+
    fn next_token(&self) -> Token {
-      todo!();
+      let token = match self.ch {
+         '=' =>
+            Lexer::new_token(TokenKind::Assign, self.ch),
+         ';' =>
+            Lexer::new_token(TokenKind::Semicolon, self.ch),
+         '(' =>
+            Lexer::new_token(TokenKind::LParen, self.ch),
+         ')' =>
+            Lexer::new_token(TokenKind::RParen, self.ch),
+         ',' =>
+            Lexer::new_token(TokenKind::Comma, self.ch),
+         '+' =>
+            Lexer::new_token(TokenKind::Plus, self.ch),
+         '{' =>
+            Lexer::new_token(TokenKind::LBrace, self.ch),
+         '}' =>
+            Lexer::new_token(TokenKind::RBrace, self.ch),
+         '\0' =>
+            Token { kind: TokenKind::Eof, literal: "".to_string() },
+         _ => Lexer::new_token(TokenKind::Illegal, self.ch),
+      };
    }
+
+    fn new_token(kind: TokenKind, ch: char) -> Token {
+        Token { kind, literal: ch.to_string() }
+    }
 }
 
 
@@ -69,6 +115,7 @@ mod tests {
             literal: "".to_string(),
          },
          ];
+
       let lexer: Lexer = Lexer::new(input);
 
       for (idx, exp_token) in expected.into_iter().enumerate() {
@@ -76,7 +123,8 @@ mod tests {
          assert_eq!(
             exp_token.kind, recv_token.kind,
             "tests[{idx}] - token type wrong. expected={}, got={}" 
-            , exp_token.kind, recv_token.kind);
+            , exp_token.kind, recv_token.kind
+            );
 
          assert_eq!(
             exp_token.literal, recv_token.literal,
